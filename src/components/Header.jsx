b69d8f28f75672c75567f91689f9d726137b3352
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-26 21:40:39 -0500

    JJBA: Paso 2 - Codigo del componente Header.jsx - Barra de Navegación superior

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1 className="logo-title">📰 Bloc de Noticias</h1>
+      </div>
+      <div className="header-tools">
+        <span className="badge-live">Actualizado al momento</span>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
