a7a1f58dba60e326d86c30aa18bb1fca768f4d32
JJBeltran1894 <juanjbeltran94@hotmail.com>
2026-06-26 21:50:44 -0500

    JJBA: Paso 3 - Conectar los componentes en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import ListaNoticias from "./components/ListaNoticias";
 
 function App() {
-  return f;
+  return (
+    <div className="app-container">
+      <Header />
+      <main className="main-content">
+        <ListaNoticias />
+      </main>
+    </div>
+  );
 }
 
 export default App;
